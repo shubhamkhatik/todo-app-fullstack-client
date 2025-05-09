5c1aa88b15ed79a2b4edbf91b987a2750a01d297
shubham khatik <40956270+shubhamkhatik@users.noreply.github.com>
2025-05-09 18:14:37 +0530

    deployment link backend  added

--- src/store/constants.js
+++ src/store/constants.js
@@ -1,3 +1,5 @@
 export const BASE_URL = location.hostname === "localhost"
     ? "http://localhost:5000/api"
-    : "https://devtinder-backend-s1q4.onrender.com/api";
+    : "https://todo-app-fullstack-server.onrender.com/api";
+
+    // export const BASE_URL = "https://todo-app-fullstack-server.onrender.com/api";
